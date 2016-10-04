a7048477266a10c2f6774117731c6258f3bc0587
林孝吉 <a3788466@gmail.com>
2016-10-04 15:34:26 +0800

    第四次修改
    Example.java加入io輸出

--- LabWebServletJdbcSpringMaven01/src/main/java/test/Example.java
+++ LabWebServletJdbcSpringMaven01/src/main/java/test/Example.java
@@ -20,6 +20,11 @@ public class Example {
 						System.out.println(iterator.next().parent().childNode(1).childNode(0).toString());
 						System.out.println(iterator.next().parent().childNode(4).childNode(0).toString());
 						break;
+//						PrintStream out = new PrintStream(new BufferedOutputStream(new FileOutputStream("C:/Users/Student/Desktop/test.txt", true)));
+//						System.setOut(out); // 轉換輸出流
+//						System.out.println(iterator.next().text()); // 將輸出傳進文件
+//						out.close();
+//						System.out.println(iterator.next().text().length());
 					}
 				}
 			}
